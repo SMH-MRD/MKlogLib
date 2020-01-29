6827dac1601e2fdc7a74c9de5a025dd1be79d2ab
Kobayashi <masato.kobayashi@shi-g.com>
2020-01-30 07:59:40 +0900

    update20200129

--- MKlog/CMKlog.cpp
+++ MKlog/CMKlog.cpp
@@ -16,73 +16,85 @@ namespace MKlog
 	ST_MKLOG_SET CMKlog::mklogset[LOG_RECORD_MAX];
 	int CMKlog::nlogs;
 	wstring CMKlog::log_path;
+	double CMKlog::def_double=0.0;
+	bool CMKlog::def_bool=false;
 
 	//########################################################################
 	int CMKlog::init_logfunc() {
 	
-	//iniファイルPATH取得
-	GetModuleFileName(NULL, szInipath, (sizeof(TCHAR) * _MAX_PATH));// exe fileのパス取り込み
-	_wsplitpath_s(szInipath, szDrive, sizeof(szDrive), szPath, sizeof(szPath), szFName, sizeof(szFName), szExt, sizeof(szExt)); // exe fileのパス分割
-	log_path.clear(); log_path += szPath;
-	_wmakepath_s(szInipath, sizeof(szInipath), szDrive, szPath, NAME_OF_INIFILE, EXT_OF_INIFILE); // ini fileのパスセット
+		for (int i = 0; i < LOG_RECORD_MAX; i++) {
+			for (int k = 0; k < LOG_ITEM_MAX; k++) {
+				mklogset[i].p_double[k] = &def_double;
+				mklogset[i].p_int[k] = &nlogs;
+				mklogset[i].p_bool[k] = &def_bool;
+			}
+		}
+		for (int i = 0; i < LOG_RECORD_MAX; i++)mklogset[i].fp = nullptr;
 
-	for (int i = 0; i < LOG_RECORD_MAX; i++)mklogset[i].fp = nullptr;
+		//iniファイルPATH取得
+		GetModuleFileName(NULL, szInipath, (sizeof(TCHAR) * _MAX_PATH));// exe fileのパス取り込み
+		_wsplitpath_s(szInipath, szDrive, sizeof(szDrive), szPath, sizeof(szPath), szFName, sizeof(szFName), szExt, sizeof(szExt)); // exe fileのパス分割
+		log_path.clear(); log_path += szPath;
+		_wmakepath_s(szInipath, sizeof(szInipath), szDrive, szPath, NAME_OF_INIFILE, EXT_OF_INIFILE); // ini fileのパスセット
 
-	//iniファイル設定値読み込み
-	TCHAR buf_t[128];
-	DWORD	str_num = GetPrivateProfileString(PATH_SECT_OF_INIFILE, PATH1_LOG1_KEY_OF_INIFILE, L"FAILED", szLogfolder1, sizeof(szLogfolder1), szInipath);
+
+		//iniファイル設定値読み込み
+		TCHAR buf_t[128];
+		DWORD	str_num = GetPrivateProfileString(PATH_SECT_OF_INIFILE, PATH1_LOG1_KEY_OF_INIFILE, L"FAILED", szLogfolder1, sizeof(szLogfolder1), szInipath);
 			log_path += szLogfolder1;
 			str_num = GetPrivateProfileString(SET_SECT_OF_INIFILE, SET_NUM_OF_LOGS_KEY_OF_INIFILE, L"FAILED", buf_t, sizeof(buf_t), szInipath); if (str_num < 2) nlogs = stoi(buf_t);
 
 			//LOG01
 			if (nlogs > 0) {
-				str_num = GetPrivateProfileString(SET_SECT_OF_INIFILE, SET_LOGNAME1_KEY_OF_INIFILE, L"FAILED", buf_t, sizeof(buf_t), szInipath);
+				str_num = GetPrivateProfileString(SET_SECT_OF_INIFILE, SET_LOGNAME1_KEY_OF_INIFILE, L"FAILED", mklogset[MK_LOGSET_1].fname, sizeof(mklogset[MK_LOGSET_1].fname), szInipath);
 				_wsplitpath_s(szInipath, szDrive, sizeof(szDrive), szPath, sizeof(szPath), szFName, sizeof(szFName), szExt, sizeof(szExt)); // exe fileのパス分割
-				_wmakepath_s(mklogset[MK_LOGSET_1].fname, sizeof(mklogset[MK_LOGSET_1].fname), szDrive, szPath, buf_t, EXT_OF_CSVFILE);
+			//	_wmakepath_s(mklogset[MK_LOGSET_1].fname, sizeof(mklogset[MK_LOGSET_1].fname), szDrive, szPath, buf_t, EXT_OF_CSVFILE);
 				str_num = GetPrivateProfileString(SET_SECT_OF_INIFILE, SET_LOGTYPE1_KEY_OF_INIFILE, L"FAILED", mklogset[MK_LOGSET_1].szType, sizeof(mklogset[MK_LOGSET_1].szType), szInipath);
 				str_num = GetPrivateProfileString(SET_SECT_OF_INIFILE, SET_SAMPLE_TIME1_KEY_OF_INIFILE, L"FAILED", buf_t, sizeof(buf_t), szInipath); if (str_num < 5)mklogset[MK_LOGSET_1].ms_period = stoi(buf_t);
-				str_num = GetPrivateProfileString(SET_SECT_OF_INIFILE, SET_ITME_NUM1_KEY_OF_INIFILE, L"FAILED", buf_t, sizeof(buf_t), szInipath); mklogset[MK_LOGSET_1].n_item = stoi(buf_t);
+				str_num = GetPrivateProfileString(SET_SECT_OF_INIFILE, SET_ITME_NUM1_KEY_OF_INIFILE, L"FAILED", buf_t, sizeof(buf_t), szInipath); if (str_num < 5) mklogset[MK_LOGSET_1].n_item = stoi(buf_t);
+				str_num = GetPrivateProfileString(SET_SECT_OF_INIFILE, SET_ITME_TIMESET1_KEY_OF_INIFILE, L"FAILED", buf_t, sizeof(buf_t), szInipath); if (str_num < 5) mklogset[MK_LOGSET_1].timeset = stoi(buf_t);
 				str_num = GetPrivateProfileString(DATA_TYPE1_SECT_OF_INIFILE, TYPE1_01_KEY_OF_INIFILE, L"FAILED", buf_t, sizeof(buf_t), szInipath); if (str_num < 5)mklogset[MK_LOGSET_1].data_type[0] = stoi(buf_t);
 				str_num = GetPrivateProfileString(DATA_TYPE1_SECT_OF_INIFILE, TYPE1_02_KEY_OF_INIFILE, L"FAILED", buf_t, sizeof(buf_t), szInipath); if (str_num < 5)mklogset[MK_LOGSET_1].data_type[1] = stoi(buf_t);
 				str_num = GetPrivateProfileString(DATA_TYPE1_SECT_OF_INIFILE, TYPE1_03_KEY_OF_INIFILE, L"FAILED", buf_t, sizeof(buf_t), szInipath); if (str_num < 5)mklogset[MK_LOGSET_1].data_type[2] = stoi(buf_t);
 			}
 			//LOG02
 			if (nlogs > 1) {
-				str_num = GetPrivateProfileString(SET_SECT_OF_INIFILE, SET_LOGNAME2_KEY_OF_INIFILE, L"FAILED", buf_t, sizeof(buf_t), szInipath);
+				str_num = GetPrivateProfileString(SET_SECT_OF_INIFILE, SET_LOGNAME2_KEY_OF_INIFILE, L"FAILED", mklogset[MK_LOGSET_2].fname, sizeof(mklogset[MK_LOGSET_2].fname), szInipath);
 				_wsplitpath_s(szInipath, szDrive, sizeof(szDrive), szPath, sizeof(szPath), szFName, sizeof(szFName), szExt, sizeof(szExt)); // exe fileのパス分割
-				_wmakepath_s(mklogset[MK_LOGSET_2].fname, sizeof(mklogset[MK_LOGSET_2].fname), szDrive, szPath, buf_t, EXT_OF_CSVFILE);
+			//	_wmakepath_s(mklogset[MK_LOGSET_2].fname, sizeof(mklogset[MK_LOGSET_2].fname), szDrive, szPath, buf_t, EXT_OF_CSVFILE);
 				str_num = GetPrivateProfileString(SET_SECT_OF_INIFILE, SET_LOGTYPE2_KEY_OF_INIFILE, L"FAILED", mklogset[MK_LOGSET_2].szType, sizeof(mklogset[MK_LOGSET_2].szType), szInipath);
 				str_num = GetPrivateProfileString(SET_SECT_OF_INIFILE, SET_SAMPLE_TIME2_KEY_OF_INIFILE, L"FAILED", buf_t, sizeof(buf_t), szInipath); if (str_num < 5)mklogset[MK_LOGSET_2].ms_period = stoi(buf_t);
-				str_num = GetPrivateProfileString(SET_SECT_OF_INIFILE, SET_ITME_NUM2_KEY_OF_INIFILE, L"FAILED", buf_t, sizeof(buf_t), szInipath); mklogset[MK_LOGSET_2].n_item = stoi(buf_t);
+				str_num = GetPrivateProfileString(SET_SECT_OF_INIFILE, SET_ITME_NUM2_KEY_OF_INIFILE, L"FAILED", buf_t, sizeof(buf_t), szInipath); if (str_num < 5) mklogset[MK_LOGSET_2].n_item = stoi(buf_t);
+				str_num = GetPrivateProfileString(SET_SECT_OF_INIFILE, SET_ITME_TIMESET2_KEY_OF_INIFILE, L"FAILED", buf_t, sizeof(buf_t), szInipath); if (str_num < 5) mklogset[MK_LOGSET_2].timeset = stoi(buf_t);
 				str_num = GetPrivateProfileString(DATA_TYPE2_SECT_OF_INIFILE, TYPE2_01_KEY_OF_INIFILE, L"FAILED", buf_t, sizeof(buf_t), szInipath); if (str_num < 5)mklogset[MK_LOGSET_2].data_type[0] = stoi(buf_t);
 				str_num = GetPrivateProfileString(DATA_TYPE2_SECT_OF_INIFILE, TYPE2_02_KEY_OF_INIFILE, L"FAILED", buf_t, sizeof(buf_t), szInipath); if (str_num < 5)mklogset[MK_LOGSET_2].data_type[1] = stoi(buf_t);
 				str_num = GetPrivateProfileString(DATA_TYPE2_SECT_OF_INIFILE, TYPE2_03_KEY_OF_INIFILE, L"FAILED", buf_t, sizeof(buf_t), szInipath); if (str_num < 5)mklogset[MK_LOGSET_2].data_type[2] = stoi(buf_t);
 			}
 			//LOG03
 			if (nlogs > 2) {
-				str_num = GetPrivateProfileString(SET_SECT_OF_INIFILE, SET_LOGNAME3_KEY_OF_INIFILE, L"FAILED", buf_t, sizeof(buf_t), szInipath);
+				str_num = GetPrivateProfileString(SET_SECT_OF_INIFILE, SET_LOGNAME3_KEY_OF_INIFILE, L"FAILED", mklogset[MK_LOGSET_3].fname, sizeof(mklogset[MK_LOGSET_3].fname), szInipath);
 				_wsplitpath_s(szInipath, szDrive, sizeof(szDrive), szPath, sizeof(szPath), szFName, sizeof(szFName), szExt, sizeof(szExt)); // exe fileのパス分割
-				_wmakepath_s(mklogset[MK_LOGSET_3].fname, sizeof(mklogset[MK_LOGSET_3].fname), szDrive, szPath, buf_t, EXT_OF_CSVFILE);
+			//	_wmakepath_s(mklogset[MK_LOGSET_3].fname, sizeof(mklogset[MK_LOGSET_3].fname), szDrive, szPath, buf_t, EXT_OF_CSVFILE);
 				str_num = GetPrivateProfileString(SET_SECT_OF_INIFILE, SET_LOGTYPE3_KEY_OF_INIFILE, L"FAILED", mklogset[MK_LOGSET_3].szType, sizeof(mklogset[MK_LOGSET_3].szType), szInipath);
 				str_num = GetPrivateProfileString(SET_SECT_OF_INIFILE, SET_SAMPLE_TIME3_KEY_OF_INIFILE, L"FAILED", buf_t, sizeof(buf_t), szInipath); if (str_num < 5)mklogset[MK_LOGSET_3].ms_period = stoi(buf_t);
-				str_num = GetPrivateProfileString(SET_SECT_OF_INIFILE, SET_ITME_NUM3_KEY_OF_INIFILE, L"FAILED", buf_t, sizeof(buf_t), szInipath); mklogset[MK_LOGSET_3].n_item = stoi(buf_t);
+				str_num = GetPrivateProfileString(SET_SECT_OF_INIFILE, SET_ITME_NUM3_KEY_OF_INIFILE, L"FAILED", buf_t, sizeof(buf_t), szInipath); if (str_num < 5) mklogset[MK_LOGSET_3].n_item = stoi(buf_t);
+				str_num = GetPrivateProfileString(SET_SECT_OF_INIFILE, SET_ITME_TIMESET3_KEY_OF_INIFILE, L"FAILED", buf_t, sizeof(buf_t), szInipath); if (str_num < 5) mklogset[MK_LOGSET_3].timeset = stoi(buf_t);
 				str_num = GetPrivateProfileString(DATA_TYPE3_SECT_OF_INIFILE, TYPE3_01_KEY_OF_INIFILE, L"FAILED", buf_t, sizeof(buf_t), szInipath); if (str_num < 5)mklogset[MK_LOGSET_3].data_type[0] = stoi(buf_t);
 				str_num = GetPrivateProfileString(DATA_TYPE3_SECT_OF_INIFILE, TYPE3_02_KEY_OF_INIFILE, L"FAILED", buf_t, sizeof(buf_t), szInipath); if (str_num < 5)mklogset[MK_LOGSET_3].data_type[1] = stoi(buf_t);
 				str_num = GetPrivateProfileString(DATA_TYPE3_SECT_OF_INIFILE, TYPE3_03_KEY_OF_INIFILE, L"FAILED", buf_t, sizeof(buf_t), szInipath); if (str_num < 5)mklogset[MK_LOGSET_3].data_type[2] = stoi(buf_t);
 			}
 			//LOG04
-			if (nlogs > 3) {}
+			if (nlogs > 3) { ; }
 			//LOG05
-			if (nlogs > 4) {}			
+			if (nlogs > 4) { ; }
 			//LOG06
-			if (nlogs > 5) {}
+			if (nlogs > 5) { ; }
 			//LOG07
-			if (nlogs > 6) {}
+			if (nlogs > 6) { ; }
 			//LOG08
-			if (nlogs > 7) {}
+			if (nlogs > 7) { ; }
 
-			wstring ws_now = getDatetimeStr();
-
+	
 		return 0;
 	};
 	//########################################################################
@@ -93,15 +105,39 @@ namespace MKlog
 		for (; logID < nlogs; logID++) if (mklogset[logID].timerID == idEvent)break;
 		if (logID >= nlogs) return;
 
-		wcout << "LOG ID :" << logID << endl;
+		wcout << "double[0] :" << *mklogset[logID].p_double[0] << endl;
 
 		if (mklogset[logID].status == LOG_STATUS_ACTIVE) add_record(logID);
 		else if ((mklogset[logID].status == LOG_STATUS_STOP) || (mklogset[logID].status == LOG_STATUS_ERROR)) {
 			KillTimer(nullptr, idEvent); mklogset[logID].timerID = 0;
+			end_record(logID);
 		}
 		else;
 		return;
 	};
+	//########################################################################
+	int CMKlog::set_label(int logID) {
+		wstring wstr; wstr.clear();
+		wstringstream ws;
+		DWORD written;
+		time_t t = time(0);
+		ws << setw(10) << t;
+
+		wstr += ws.str();
+		wstr += L",";
+		wstr += L"LABEL1";
+		wstr += L"\r\n";
+		WriteFile(mklogset[logID].hf, wstr.c_str(), wstr.length() * 2, &written, NULL);
+		
+		wstr.clear();
+		wstr += ws.str();
+		wstr += L",";
+		wstr += L"DATA1";
+		wstr += L"\r\n";
+		WriteFile(mklogset[logID].hf, wstr.c_str(), wstr.length()*2, &written, NULL);
+
+		return 0;
+	}
 
 	//########################################################################
 	int CMKlog::start_record(int logID, HWND hWnd) {
@@ -109,21 +145,34 @@ namespace MKlog
 		
 		mklogset[logID].status = LOG_STATUS_ACTIVE;
 
+		//ファイル名設定
+		mklogset[logID].filename.clear();
+		mklogset[logID].filename += log_path;
+		mklogset[logID].filename += mklogset[logID].fname;
+		mklogset[logID].filename += DELIMITER_OF_FILE;
+		mklogset[logID].filename += getDatetimeStr();
+		mklogset[logID].filename += EXT_OF_CSVFILE;
+		//ログファイルオープン
+		mklogset[logID].hf = CreateFile(mklogset[logID].filename.c_str(), GENERIC_WRITE, 0, NULL, OPEN_ALWAYS, 0, NULL);
+		//ラベル記録
+		set_label(logID);
+		//ログ記録タイマーセット
 		if (mklogset[logID].ms_period > 9) {
 			if (hWnd != NULL) mklogset[logID].timerID = SetTimer(hWnd, TIMER_ID_BASE+logID, mklogset[logID].ms_period, NULL);
 			else              mklogset[logID].timerID = SetTimer(NULL, NULL, mklogset[logID].ms_period, MKlogTimerProc);
 		}
 
 		return 0;
-	};
+};
 
 	//########################################################################
 	int CMKlog::end_record(int logID) {
 		if ((logID > nlogs) || (logID < 0))return -1;
 		mklogset[logID].status = LOG_STATUS_STOP;
-		if (mklogset[logID].fp != nullptr) {
-			fclose(mklogset[logID].fp); mklogset[logID].fp = nullptr;
-		}
+		KillTimer(nullptr, mklogset[logID].timerID); 
+		mklogset[logID].timerID = 0;
+
+		if(mklogset[logID].hf!=NULL) CloseHandle(mklogset[logID].hf);
 		return 0;
 	};
 
@@ -142,13 +191,13 @@ namespace MKlog
 
 	//########################################################################
 	wstring CMKlog::getDatetimeStr() {
-	
+		static std::wstringstream ws;
 		time_t t = time(nullptr);
 		struct tm t_now;
 		errno_t error = localtime_s(&t_now, &t);
 
-		std::wstringstream ws;
-		ws << L"20" << t_now.tm_year - 100;
+		ws.clear(); ws.str(L"");
+	//	ws << L"20" << t_now.tm_year - 100;
 		// setw(),setfill()で0詰め
 		ws << setw(2) << setfill(L'0') << t_now.tm_mon + 1;
 		ws << setw(2) << setfill(L'0') << t_now.tm_mday;
--- MKlog/CMKlog.h
+++ MKlog/CMKlog.h
@@ -15,13 +15,19 @@ namespace MKlog
 #define LOG_STATUS_STOP		3 //LOG記録の状態
 #define LOG_STATUS_ERROR	-1 //LOG記録の状態
 
+#define EXT_OF_CSVFILE			L".csv"		//csvファイル拡張子
+#define DELIMITER_OF_FILE		L"_"		//ファイル名区切り文字
+
 
 	typedef struct stMKlogSet {
 		FILE*	fp;
-		WCHAR	fname[256];
+		HANDLE	hf;
 		WCHAR	szType[128];				//BASIC:定周期で記録し続ける　TRAP:定周期で指定数サンプリング　EVENT：イベントトリガで記録
+		WCHAR	fname[256];					//ファイル名
+		std::wstring filename;
 		int		ms_period;					//サンプリング周期　msec NEGATIVE VALUE:イベント型
 		int		n_item;						//サンプリングデータ項目数
+		int		timeset;					//時間記録タイプ
 		int		data_type[LOG_ITEM_MAX];	//0:double 1:int 2:bool
 		double*	p_double[LOG_ITEM_MAX];		//記録データのアドレスマップ
 		int*	p_int[LOG_ITEM_MAX];		//記録データのアドレスマップ
@@ -43,20 +49,26 @@ namespace MKlog
 		static	int nlogs;
 		static  std::wstring log_path;
 		static  HWND hwnd_client;
+		static	double def_double;	//参照ポインタ初期設定用　ダミーデータ
+		static	bool def_bool;		//参照ポインタ初期設定用　ダミーデータ
 
-
-		
 		int init_logfunc();//初期化　設定フィル読み込み
 
 		static int start_record(int logID, HWND hWnd);//int logID, HWND hWnd:クライアント側でタイマー処理をする場合のイベント受信Window
 		static int end_record(int logID);
 		static int pause_record(int logID);
 		static int add_record(int logID);
+		static int set_label(int logID);
+
+		int set_double_data(double* pd, int logID, int i_item) { if((logID<LOG_RECORD_MAX)&&(i_item<LOG_ITEM_MAX)) mklogset[logID].p_double[i_item] = pd; return 0; };
+		int set_int_data(int* pi, int logID, int i_item) { if ((logID<LOG_RECORD_MAX) && (i_item<LOG_ITEM_MAX)) mklogset[logID].p_int[i_item] = pi; return 0; };
+		int set_bool_data(bool* pb, int logID, int i_item) { if ((logID<LOG_RECORD_MAX) && (i_item<LOG_ITEM_MAX)) mklogset[logID].p_bool[i_item] = pb; return 0; };
+
 
 		static VOID CALLBACK MKlogTimerProc(HWND hwnd, UINT msg, UINT idEvent, DWORD dwTime);
 
 
-		std::wstring getDatetimeStr();
+		static std::wstring getDatetimeStr();
 	};
 
 
